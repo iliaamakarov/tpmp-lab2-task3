42acb29f92db5dabd69fe2c99d1661d09e76f197
iliaamakarov <iliaamakarov@gmail.com>
2026-03-03 22:13:32 +0300

    brb going to sleep

--- src/main.c
+++ src/main.c
@@ -3,13 +3,23 @@
 #include <time.h>
 
 
-struct ABBADON {
+struct ABADDON {
     char* fullName;
     char* number;
-    time_t registrationDate;
+    struct tm registrationDate;
     double balance;
     double tarif;
 };
+
+int compare(struct ABADDON* a, struct ABADDON* b) {
+    return strcomp(a, b);
+}
+
+void print(struct ABADDON* a) {
+    printf(a->fullName);
+    printf(a->number);
+    printf(a->registrationDate)
+}
 
 
 int main(int argc, char* argv[])
@@ -18,7 +28,7 @@ int main(int argc, char* argv[])
         //get n
         char* p;
         const long converted = strtol(argv[1], &p, 10);	//10 - base
-        if (errno != 0 || *p != '\0' || converted > INT_MAX || converted < INT_MIN) {
+        if (errno != 0 || *p != '\0' || converted > 12 || converted < 0) {
             printf("Incorrect command line argument for array size: %ld", converted);
             return -1;
         }
@@ -26,24 +36,39 @@ int main(int argc, char* argv[])
         printf("%d\n", n);
         //
         //array
-        struct ABBADON* arr = calloc(n, sizeof(struct ABBADON));
-        char* time = malloc(10, sizeof(char));
+        struct ABADDON* arr = calloc(n, sizeof(struct ABADDON));
+        char* temptime = malloc(11, sizeof(char));
         printf("I couldn't bother handling errors for each input so type correctly pls");
         for (unsigned int i = 0; i < n; i++) {
-            memset(&time, 0, sizeof(time));
+            memset(&temptime, 0, sizeof(temptime));
             printf("Enter name and initials: ");
             scanf("%s", &(arr[i].fullName));
             printf("Enter number: ");
             scanf("%s", &(arr[i].number));
-            printf("Enter registration date(yyyy mm dd): ");
-            scanf("%s", &time);
-            strptime(time, "%Y:%m:%d", (arr[i].registrationDate));  //adresses or just as is?
+            printf("Enter registration date(yyyy:mm:dd): ");
+            scanf("%s", &temptime);
+            strptime(temptime, "%Y:%m:%d", &(arr[i].registrationDate));  //adresses or just as is?
             printf("Enter balance: ");
             scanf("%lf", &(arr[i].balance));
             printf("Enter tariff: ");
             scanf("%lf", &(arr[i].tarif));
         }
 
+        qsort(arr, n, sizeof(struct ABADDON), compare);
+
+        time_t mytime = time(NULL);
+        struct tm* now = localtime(&mytime);
+        for (unsigned int i = 0; i < n; i++) {
+            if (now->tm_year - arr[i].registrationDate.tm_year >= 10) {
+                arr[i].balance += 20.0;
+            }
+        }
+
+        for (unsigned int i = 0; i < n; i++) {
+            if (arr[i].balance - arr[i].tarif < 0.0) {
+
+            }
+        }
     }
     else {
         printf("Incorrect number of arguments.");
